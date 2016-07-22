8c575d26aed37e4203264100fc512e879cb42918
Pablo Fonseca <pablofonseca777@gmail.com>
2016-07-22 06:59:05 -0300

    Validate encoding

--- vendor/assets/javascripts/jquery.validationEngine-es.js
+++ vendor/assets/javascripts/jquery.validationEngine-es.js
@@ -130,7 +130,11 @@
                 },
                 "validate2fields": {
                     "alertText": "* Por favor entrar HELLO"
-                }
+                },
+                "validate_encoding": {
+                    "regex": /(?![\x00-\x7F]|[\xC0-\xDF][\x80-\xBF]|[\xE0-\xEF][\x80-\xBF]{2}|[\xF0-\xF7][\x80-\xBF]{3})./g
+                    "alertText": "* Este campo tiene caracteres inválidos",
+                },
             };
             
         }
